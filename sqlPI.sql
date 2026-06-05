8b04311c9b278157682a8ce13c5bbb12475afb72
PedroOSanches <pedrohenriqueoliveirasanches3@gmail.com>
2026-06-05 16:52:39 -0300

    Add Select Progresso Aluno

--- sqlPI.sql
+++ sqlPI.sql
@@ -1,3 +1,4 @@
+-- Active: 1779975789762@@pi-gameficacao-db-pedrohenriqueoliveirasanches3-0b27.i.aivencloud.com@16095@jornadamauadb
 -- ======================= Adicionar Usuario/Aluno ===================== --
 INSERT INTO
     usuario (
@@ -228,7 +229,20 @@ INSERT INTO resposta_upload(
 commit;
 
 -- ============================== Registro Nova Secao do Tabuleiro ==============================
-INSERT INTO secao(titulo_secao, descricao_secao) VALUES('Explorador', 'Descricao')
+INSERT INTO secao(titulo_secao, descricao_secao) VALUES('Explorador', 'Descricao');
 
 -- ============================== Registro Nova Casa ==============================
-INSERT INTO casa(id_secao, titulo_casa, data_limite_casa) VALUES (1, 'Titulo Casa', '2026-06-05')
+INSERT INTO casa(id_secao, titulo_casa, data_limite_casa) VALUES (1, 'Titulo Casa', '2026-06-05');
+
+-- ============================== Select Progresso Aluno ==============================
+SELECT 
+    tarefas_concluidas,
+    total_tarefas,
+    ROUND(tarefas_concluidas * 100.0 / total_tarefas, 2) as progresso_aluno
+FROM (
+    SELECT 
+        COUNT(DISTINCT id_tarefa) as tarefas_concluidas,
+        (SELECT COUNT(*) FROM tarefa) as total_tarefas
+    FROM tentativa
+    WHERE id_usuario = 16
+) resultado;
